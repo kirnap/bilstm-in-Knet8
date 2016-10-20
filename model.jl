d841ec9a9eb4cdf9bd111161571aada890c2b648
OmerKirnap <okirnap@ku.edu.tr>
2016-10-20 20:01:10 +0300

    bilstm without merge

--- model.jl
+++ model.jl
@@ -1,4 +1,7 @@
-
+@doc """ Bi-directional LSTM for language modeling, training procedure:
+         Suppose an input sequence of x1-x2-x3,
+         The backward lstm gets input as </s>-x3-x2,
+         The forward lstm gets input as <s>-x1-x2 """
 
 """
 parameters: holds the weight and bias for kth lstm layer as parameters[2k-1,2k]: weight and bias.
@@ -9,12 +12,13 @@ type LSTM
     parameters::Array
     nlayers::Int
     states::Array
+    atype::DataType
 
     function LSTM(atype, layerconfig, embedsize, batchsize, winit)
         nlayers = length(layerconfig)
         parameters = initweights(atype, layerconfig, embedsize, winit)
         states = initstate(atype, layerconfig, batchsize)
-        new(parameters, nlayers, states)
+        new(parameters, nlayers, states, atype)
     end
 end
 
@@ -22,6 +26,10 @@ end
 # No need for embedding matrix TODO: lookup table implementation
 """
 Initialized the weights and biases for the stacked LSTM.
+atype: Array type of the machine, namely KnetArray or Array regarding of gpu usage
+hidden: Array of hidden layer configurations, e.g, [128 256] 2 layers of lstm.
+embed: Embedding size of the model
+winit: initialization parameter of the model
 """
 function initweights(atype, hidden, embed, winit)
     param = Array(Any, 2*length(hidden))
@@ -50,9 +58,9 @@ end
 
 
 """
-forward implementation for single layer lstm
+forward implementation for a single layer lstm
 """
-function lstmforward(weight, bias, hidden, cell, input)
+function lstm(weight, bias, hidden, cell, input)
     gates   = hcat(input,hidden) * weight .+ bias
     hsize   = size(hidden,2)
     forget  = sigm(gates[:,1:hsize])
@@ -65,4 +73,43 @@ function lstmforward(weight, bias, hidden, cell, input)
 end
 
 
+"""
+Go one step forward for a given input to the stacked lstm,
+input: in language modeling input is a single token
+returns the last hidden layer of the stacked lstm
+"""
+function sforw(l::LSTM, input)
+    # TODO check with the l.states modification
+    x = input # input is the embeded version of the original vocabulary
+    for i=1:2:length(l.states)
+        (l.states[i], l.states[i+1]) = lstm(l.parameters[i], l.parameters[i+1], l.states[i], l.states[i+1], x)
+        x = l.states[i]
+    end
+    return l.states[end-1] 
+end
+
+
 # TODO: Merge layer will handle the final prediction parameters, namely weight and bias
+"""
+Go forward and collect hidden states of the lstm for a sequence
+sequence is padded from begining with token <s> and end with token </s>
+returns hiddenlayers: contains the final hidden states of the forward and backward lstms in correct order,
+
+"""
+function bilsforw(net::LSTM, sequence; forwardlstm=true)
+    hiddenlayers = Array(Any, length(sequence))
+
+    # if forward first item else the last item will be zeros of the same size with other items
+    traverse = (forwardlstm ? (2:length(sequence)) : (length(sequence)-1:-1:1))
+
+    for i=traverse
+        result = sforw(net, sequence[i])
+        hiddenlayers[i] = result
+    end
+
+    padindex = (forwardlstm ? 1 : end)
+    padding = convert(atype, zeros(eltype(hiddenlayers[3]), size(hiddenlayers[3])))
+    hiddenlayers[padindex] = padding    
+end
+# TODO : test the bilstm forward implementation
+
