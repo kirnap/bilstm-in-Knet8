42e7612629fb3ef6291fbe3101c6edb9ff59ce33
OmerKirnap <okirnap@ku.edu.tr>
2016-11-02 21:00:03 +0300

    create data type

--- preprocess.jl
+++ preprocess.jl
@@ -9,6 +9,51 @@ const EOS = "</s>"
 const UNK = "<unk>"
 const LLIMIT = 2
 
+type Data
+    word_to_index::Dict{AbstractString, Int}
+    index_to_word::Vector{AbstractString}
+    batchsize::Int32
+    serve_type::AbstractString
+    sequences::Dict
+end
+
+function Data(datafile; word_to_index=nothing, vocabfile=nothing, serve_type=nothing, batchsize=20)
+    (serve_type == nothing) && error("Please specify the data serve type: onehot, bitarray or sequence")
+    existing_vocab = (word_to_index != nothing)
+    if !existing_vocab
+        word_to_index = Dict{AbstractString, Int}(SOS=>1, EOS=>2, UNK=>3)
+        vocabfile != nothing && (V = vocab_from_file(vocabfile))
+    end
+
+    stream = open(datafile)
+    sequences = Dict{Int64, Array{Any, 1}}()
+    for line in eachline(stream)
+        words = Int32[]
+        push!(words, word_to_index[SOS])
+        for word in split(line)
+            if !existing_vocab && vocabfile != nothing && !(word in V)
+                word = UNK
+            end
+            if existing_vocab
+                index = get(word_to_index, word, word_to_index[UNK])
+            else
+                index = get!(word_to_index, word, 1+length(word_to_index))
+            end
+            push!(words, index)
+        end
+        push!(words, word_to_index[EOS])
+        skey = length(words)
+        (!haskey(sequences, skey)) && (skey > LLIMIT) && (sequences[skey] = Any[])
+        (skey != LLIMIT) && push!(sequences[skey], words)
+    end
+    close(stream)
+    vocabsize = length(word_to_index)
+    index_to_word = Array(AbstractString, vocabsize)
+    for (word, index) in word_to_index
+        index_to_word[index] = word
+    end
+    Data(word_to_index, index_to_word, batchsize, serve_type, sequences)
+end
 
 
 
