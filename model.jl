08411ec0c2f503185577c0ba02cf6827435a0673
OmerKirnap <okirnap@ku.edu.tr>
2016-12-13 11:16:25 +0300

    create embedding initialization function

--- model.jl
+++ model.jl
@@ -30,6 +30,8 @@ atype: Array type of the machine, namely KnetArray or Array regarding of gpu usa
 hidden: Array of hidden layer configurations, e.g, [128 256] 2 layers of lstm.
 embed: Embedding size of the model
 winit: initialization parameter of the model
+OUTPUT:
+param[2k-1, 2k]: weight and bias for the kth lstm layer
 """
 function initweights(atype, hidden, embed, winit)
     param = Array(Any, 2*length(hidden))
@@ -110,14 +112,12 @@ function bilsforw(parameters, states, sequence; forwardlstm=true)
     return hiddenlayers
 end
 
-
 """
-embedding : <vocab x embed> size matrix each row contains one of the related words
-minibatch : contains batchsize many nth words of the sequences
+Embedding matrix initialization it is of the size (vocab x embed)
+The input to lstm units would be x * W where W is embedding and x is traditional one-hots.
 """
-function lookup(embedding, minibatch, atype)
-    result = embedding[minibatch, :]
-    return convert(atype, result)
+function initembedding(atype, embedsize, vocabsize, winit)
+    return convert(atype, winit*randn(vocabsize, embedsize))
 end
 
 
@@ -145,7 +145,17 @@ function loss(paramdict, statedict, sequence, atype)
 end
 
 # Here are the remaining TODOs:
-# - Implement look up from embedding matrix. DONE
 # - Check with the weight initializations s.t. the embedding matrix is still embedding, (KnetArray support?)
 # - Implement loss function to do that flatten the final prediction matrix s.t. when we choose the correct entries of final prediction matrix
 # - Based on the above steps implement the loss function
+
+
+# FUTURE CODE: In case Knet supports table lookup
+# """
+# embedding : <vocab x embed> size matrix each row contains one of the related words
+# minibatch : contains batchsize many nth words of the sequences
+# """
+# function lookup(embedding, minibatch, atype)
+#     result = embedding[minibatch, :]
+#     return convert(atype, result)
+# end
