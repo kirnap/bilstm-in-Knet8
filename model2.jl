8ea26ac31ee8e65882fa0971174628c8eb9530a8
OmerKirnap <okirnap@ku.edu.tr>
2016-12-23 14:15:43 +0300

    add state initialization

--- model2.jl
+++ model2.jl
@@ -1,5 +1,6 @@
 # 1:2*length(layerconfig) -> forward lstm
 # 2*lenghth(layerconfig)+1:4*length(layerconfig) -> bacward lstm
+# final 3 or 4 is embedding and final layer prediction parameters
 function initparams(atype, layerconfig, embedsize, vocab, winit; single_embedding=false)
     if !single_embedding
         parameters = Array(Any, 4*length(layerconfig)+4)
@@ -32,4 +33,33 @@ function initparams(atype, layerconfig, embedsize, vocab, winit; single_embeddin
         input = layerconfig[k]
     end
     return map(p->convert(atype, p), parameters)
+end
+
+
+# 1:2*length(layerconfig) -> forward lstm
+# 2*length(layerconfig)+1:4*length(layerconfig) -> backward lstm
+function initstates(atype, layerconfig, batchsize)
+    states = Array(Any, 4*length(layerconfig))
+    len = length(layerconfig)
+
+    # forward layer initialization
+    for k=1:len
+        states[2k-1] = zeros(batchsize, layerconfig[k])
+        states[2k] = zeros(batchsize, layerconfig[k])
+    end
+
+    # backward layer initialization
+    for k=1:len
+        states[2k-1+2len] = zeros(batchsize, layerconfig[k])
+        states[2k+2len] = zeros(batchsize, layerconfig[k])
+    end
+    return map(s->convert(atype, s), states)
+end
+
+
+function resetstate!(states, atype)
+    for k=1:length(states)
+        states[k] = zeros(size(states))
+        convert(atype, states[k])
+    end
 end
