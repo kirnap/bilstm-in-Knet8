eceb02ec62c0d0b3dfe2d382deae9624190dd369
OmerKirnap <okirnap@ku.edu.tr>
2016-12-23 12:25:44 +0300

    fix embedding initialization

--- model2.jl
+++ model2.jl
@@ -1,18 +1,17 @@
-using Knet
-include("model.jl")
-include("preprocess.jl")
-
-
 # 1:2*length(layerconfig) -> forward lstm
 # 2*lenghth(layerconfig)+1:4*length(layerconfig) -> bacward lstm
 function initparams(atype, layerconfig, embedsize, vocab, winit; single_embedding=false)
     if !single_embedding
         parameters = Array(Any, 4*length(layerconfig)+4)
-        parameters[end-1] = winit * randn(vocab, embedsize) # forward lstm embedding
         parameters[end] = winit * randn(vocab, embedsize) # backward lstm embedding
+        parameters[end-1] = winit * randn(vocab, embedsize) # forward lstm embedding
+        parameters[end-2] =  winit * randn(layerconfig[end], vocab)# final layer weight
+        parameters[end-3] = zeros(1, vocab) # final layer bias
     else
         parameters = Array(Any, 4*length(layerconfig)+3)
         parameters[end] = winit * randn(vocab, embedsize) # embedding shared among forward and backward lstm
+        parameters[end-1] =  winit * randn(layerconfig[end], vocab)# final layer weight
+        parameters[end-2] = zeros(1, vocab) # final layer bias
     end
 
     len = length(layerconfig)
@@ -32,7 +31,5 @@ function initparams(atype, layerconfig, embedsize, vocab, winit; single_embeddin
         parameters[2k+2len][1:layerconfig[k]] = 1 # forget gate bias
         input = layerconfig[k]
     end
-    parameters[end-2] =  winit * randn(layerconfig[end], vocab)# final layer weight
-    parameters[end-3] = zeros(1, vocab) # final layer bias
     return map(p->convert(atype, p), parameters)
 end
